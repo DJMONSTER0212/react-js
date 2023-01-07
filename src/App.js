1c5ad7727f0ad895f0c72dc98e69efb94f276db7
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-01-07 23:38:45 +0530

    learnt how to handle event and how to  set a state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (    //react k ander jsx mai app sirf ek hi element return kar sakte hai  
   // <> it is know as jsx fragmants and we use this to send multiple elements  
   <>
-   <Navbar title="DJ's Creation 🎮" AboutText="About Me"/>
+  <Navbar title="DJ's Creation 🎮" AboutText="About Me"/>
+  <div className="container my-3">
+    <TextForm heading="Enter the text to Analyze 👇" />
+  </div>
   </>
   );
 }
