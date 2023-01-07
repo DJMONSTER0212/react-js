e57900924c719862149c670fc905552ce45a9891
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-01-07 18:53:22 +0530

    added first component i.e is navbar|| The name of the component file should always start with capital letters||

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (    //react k ander jsx mai app sirf ek hi element return kar sakte hai  
   // <> it is know as jsx fragmants and we use this to send multiple elements  
   <>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
-      <div className="container-fluid">
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a className="navbar-brand" href="/">DJ's Creation 🎮</a>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">About</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+   <Navbar/>
   </>
   );
 }
