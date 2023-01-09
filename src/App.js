d437566b69b1ef5ebfab456a7b0db84553d57d11
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-01-09 23:39:05 +0530

    learnt about light mode and dark mode and came to know how to compare javascript objects easily.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+// import About from './components/About';
 
 function App() {
+  const [darkmode,setDarkMode] = useState(false);  /// wether dark mode is enabled or not
   return (    //react k ander jsx mai app sirf ek hi element return kar sakte hai  
   // <> it is know as jsx fragmants and we use this to send multiple elements  
   <>
   <Navbar title="DJ's Creation 🎮" AboutText="About Me"/>
   <div className="container my-3">
     <TextForm heading="Enter the text to Analyze 👇" />
+    {/* <About/> */}
   </div>
   </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,20 +6,37 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLowClick = ()=>{
+        // console.log("UpperCase was clicked"+ text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange  = (event)=>{
         // console.log("On change")
         setText(event.target.value);
     }
-    const [text, setText] = useState("Enter Text here");  //text is a variable , setText is a function|| sabse pehle imported use state hook from react || text k ander = "Enter text here" reh jaayega , default value hai aur jab bhi text ko update karenge toh is function k saath karenge
+    const cleatText = ()=>{
+      setText("");
+    }
+    const [text, setText] = useState("");  //text is a variable , setText is a function|| sabse pehle imported use state hook from react || text k ander = "Enter text here" reh jaayega , default value hai aur jab bhi text ko update karenge toh is function k saath karenge
     // text  = "new text" // wrong way to change the state
     // setText("new Text"); // correct way to change the state
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1 className='my-3'>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" onChange={handleOnChange} value={text} id="mybox" rows="8"></textarea>
         </div>
-        <button className="btn btn-outline-success" onClick={handleUpClick}>Convert To Uppercase</button>
+        <button className="btn btn-outline-success mx-2" onClick={handleUpClick}>Convert To UpperCase</button>
+        <button className="btn btn-outline-primary mx-2" onClick={handleLowClick}>Convert To LowerCase</button>
+        <button className="btn btn-outline-danger mx-2" onClick={cleatText}>Clear</button>
     </div>
+    <div className="container my-2">
+      <h1>Your Text Summary</h1>
+      <p>{text.split(" ").length -1} words, {text.length} characters</p>
+      {/* <h2>Preview</h2   */}
+    </div>
+    </>
   )
 }
